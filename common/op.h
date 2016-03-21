8f74e85764a68e39865a68d6baff0f52e3f9f825
Benjamin Grange <benjamin.grange@epitech.eu>
2016-03-21 20:01:24 +0000

    Updated op.h

--- common/op.h
+++ common/op.h
@@ -5,94 +5,89 @@
 ** Login   <grange_c@epitech.net>
 **
 ** Started on  Mon Feb 29 23:37:53 2016 Benjamin Grange
-** Last update Tue Mar  1 10:38:13 2016 Benjamin Grange
+** Last update Mon Mar 21 20:00:58 2016 Benjamin Grange
 */
 
 #ifndef _OP_H_
 # define _OP_H_
 
-#define MEM_SIZE                (6 * 1024)
-#define IDX_MOD                 512   /* modulo de l'index < */
-#define MAX_ARGS_NUMBER         4     /* this may not be changed 2^*IND_SIZE */
+# define MEM_SIZE		(6 * 1024)
+# define IDX_MOD		512   /* modulo de l'index < */
+# define MAX_ARGS_NUMBER	4     /* this may not be changed 2^*IND_SIZE */
 
-#define COMMENT_CHAR            '#'
-#define LABEL_CHAR              ':'
-#define DIRECT_CHAR             '%'
-#define SEPARATOR_CHAR          ','
+# define COMMENT_CHAR		'#'
+# define LABEL_CHAR		':'
+# define DIRECT_CHAR		'%'
+# define SEPARATOR_CHAR		','
 
-#define LABEL_CHARS             "abcdefghijklmnopqrstuvwxyz_0123456789"
+# define LABEL_CHARS		"abcdefghijklmnopqrstuvwxyz_0123456789"
 
-#define NAME_CMD_STRING         ".name"
-#define COMMENT_CMD_STRING      ".comment"
-
-/*
- ** regs
- */
-
-#define REG_NUMBER      16              /* r1 <--> rx */
+# define NAME_CMD_STRING	".name"
+# define COMMENT_CMD_STRING	".comment"
 
 /*
- **
- */
+** Registres
+*/
 
-typedef char    args_type_t;
+# define REG_NUMBER		16
 
-#define T_REG           1       /* registre */
-#define T_DIR           2       /* directe  (ld  #1,r1  met 1 dans r1) */
-#define T_IND           4       /* indirecte toujours relatif
+typedef char			args_type_t;
+
+# define T_REG			1       /* registre */
+# define T_DIR			2       /* directe  (ld  #1,r1  met 1 dans r1) */
+# define T_IND			4       /* indirecte toujours relatif
                                    ( ld 1,r1 met ce qu'il y a l'adress (1+pc)
                                    dans r1 (4 octecs )) */
-#define T_LAB           8       /* LABEL */
+# define T_LAB			8       /* LABEL */
 
-struct  op_s
+/*
+** op_tab
+*/
+
+typedef struct			s_op
 {
-   char         *mnemonique;
-   char         nbr_args;
-   args_type_t  type[MAX_ARGS_NUMBER];
-   char         code;
-   int          nbr_cycles;
-   char         *comment;
+   char				*mnemonique;
+   char				nbr_args;
+   args_type_t			type[MAX_ARGS_NUMBER];
+   char				code;
+   int				nbr_cycles;
+   char				*comment;
+}				t_op;
+
+extern t_op			op_tab[];
+
+/*
+** size
+*/
+
+# define IND_SIZE		2
+# define REG_SIZE		4
+# define DIR_SIZE		REG_SIZE
+
+/*
+** header
+*/
+
+# define PROG_NAME_LENGTH        128
+# define COMMENT_LENGTH          2048
+# define COREWAR_EXEC_MAGIC      0xea83f3
+
+struct				header_s
+{
+   int				magic;
+   char				prog_name[PROG_NAME_LENGTH + 1];
+   int				prog_size;
+   char				comment[COMMENT_LENGTH + 1];
 };
 
-typedef struct op_s     op_t;
+typedef struct header_s		header_t;
 
 /*
- ** size
- */
+** live
+*/
 
-#define IND_SIZE        2               /* en octet */
-#define REG_SIZE        4               /* en octet */
-#define DIR_SIZE        REG_SIZE        /* en octet */
-
-/*
- ** op_tab
- */
-
-extern  op_t    op_tab[];
-
-/*
- ** header
- */
-#define PROG_NAME_LENGTH        128
-#define COMMENT_LENGTH          2048
-
-struct header_s
-{
-   int  magic;
-#define COREWAR_EXEC_MAGIC      0xea83f3        /* why not */
-   char prog_name[PROG_NAME_LENGTH + 1];
-   int  prog_size;
-   char comment[COMMENT_LENGTH + 1];
-};
-
-typedef struct header_s header_t;
-
-/*
- ** live
- */
-
-#define CYCLE_TO_DIE    1536    /* nombre de cycle pour etre d\'eclarer mort */
-#define CYCLE_DELTA     5
-#define NBR_LIVE        40
+# define CYCLE_TO_DIE		1536
+# define CYCLE_DELTA		5
+# define NBR_LIVE		40
 
 #endif
