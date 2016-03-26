38c75e7627720ad5645a253496c81d1c5d2e07b3
Flora Huot <flora.huot@epitech.eu>
2016-03-26 17:19:47 +0100

    [messages] Win & alive done.

--- corewar/include/program.h
+++ corewar/include/program.h
@@ -46,4 +46,7 @@ t_err	add_process(t_program *program, t_info_list *list);
 t_err	program_init(t_program *program, struct s_application *app,
 		     t_info_list *list);
 
+void	message_alive(t_program *program, int player);
+void	message_win(t_program *program, int player);
+
 #endif /* !PROGRAM_H_ */
--- corewar/source/run/run.c
+++ corewar/source/run/run.c
@@ -95,6 +95,7 @@ t_err	remove_dead(t_application *app)
   i = -1;
   while (alive <= 1 && ++i < app->program_amount)
     if (last_live == app->programs[i].last_live_cycle)
+      message_win(&app->programs[i], i);
       /* FIXME Flora : dire que ce programme a gagné! GG! (ignore le code crade please)*/;
   app->last_limit_hit = app->cycle;
   return (ERROR_UNKNOWN * (alive <= 1));
