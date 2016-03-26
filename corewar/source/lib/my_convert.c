a62b2df4ede4ebb4a8f6a4d16e9854018c165bb2
Jakob Kellendonk <jakob.kellendonk@epitech.eu>
2016-03-26 21:30:29 +0100

    my_convert en 2s-signed

--- corewar/source/lib/my_convert.c
+++ corewar/source/lib/my_convert.c
@@ -5,31 +5,33 @@
 ** Login   <kellen_j@epitech.net>
 ** 
 ** Started on  Tue Mar 22 17:50:32 2016 Jakob Kellendonk
-** Last update Sat Mar 26 19:44:35 2016 Jakob Kellendonk
+** Last update Sat Mar 26 21:29:22 2016 Jakob Kellendonk
 */
 
 int	char_to_int(unsigned char *src)
 {
-  return (((src[0] >> 7) * -2 + 1)
-	  * (((src[0] & 127) << 24)
-	     + (src[1] << 16)
-	     + (src[2] << 8)
-	     + src[3] + (src[0] >> 7)));
+  if (src[0] >> 7)
+    return (-((~(((src[0]) << 24u) + (src[1] << 16u)
+		 + (src[2] << 8u) + src[3] - 1u))));
+  return ((src[0] << 24) + (src[1] << 16) + (src[2] << 8) + src[3]);
 }
 
 int	char_to_short(unsigned char *src)
 {
-  return (((src[0] >> 7) * -2 + 1)
-	  * (((src[0] & 127) << 8)
-	     + src[1] + (src[0] >> 7)));
+  if (src[0] >> 7)
+    return (-((~(((src[0]) << 8u) + src[1] - 1u)) & 0xFFFFu));
+  return ((src[0] << 8u) + src[1]);
 }
 
 void		int_to_char(int nb, unsigned char *str)
 {
   unsigned int	abs;
 
-  abs = (nb + (nb < 0)) * ((nb > 0) * 2 - 1);
-  str[0] = ((nb < 0) * 128u) | (abs >> 24u);
+  if (nb < 0)
+    abs = ~(unsigned int)((nb + (nb < 0)) * ((nb > 0) * 2 - 1));
+  else
+    abs = nb;
+  str[0] = (abs >> 24u);
   str[1] = (abs >> 16u) & 255u;
   str[2] = (abs >> 8u) & 255u;
   str[3] = (abs & 255u);
@@ -39,7 +41,10 @@ void		short_to_char(int nb, unsigned char *str)
 {
   unsigned int	abs;
 
-  abs = (nb + (nb < 0)) * ((nb > 0) * 2 - 1);
-  str[0] = ((nb < 0) * 128u) | (abs >> 8u);
+  if (nb < 0)
+    abs = ~(unsigned int)((nb + (nb < 0)) * ((nb > 0) * 2 - 1));
+  else
+    abs = nb;
+  str[0] = (abs >> 8u) & 255u;
   str[1] = (abs & 255u);
 }
