33cd4116607e967bc7f5e7aecd5f8a02094d307e
Flora Huot <flora.huot@epitech.eu>
2016-03-27 23:02:00 +0200

    [program.c - address.c] Norme.

--- corewar/source/core/program.c
+++ corewar/source/core/program.c
@@ -15,47 +15,6 @@
 #include "my_mem.h"
 #include "read_helper.h"
 
-t_err		address_check(t_application *app, t_program *program,
-			    t_info_list *list)
-{
-  int		i;
-
-  i = 0;
-  while (app->programs + i != program)
-    {
-      if (((app->programs[i].processes[0].pc - list->address)
-	   % MEM_SIZE + MEM_SIZE) % MEM_SIZE < program->info.prog_size
-	  || ((list->address - app->programs[i].processes[0].pc)
-	      + MEM_SIZE) % MEM_SIZE
-	  < app->programs[i].info.prog_size)
-	return (print_error(ERROR_OVERLAP));
-      i = i + 1;
-    }
-  return (0);
-}
-
-t_err		set_address(t_application *app, t_program *program,
-			    t_info_list *list)
-{
-  int		i;
-  t_info_list	*tmp;
-  int		d;
-
-  i = 0;
-  tmp = list;
-  while (tmp->address == -1 && tmp->file_name && (tmp = tmp->next))
-    i = i + 1;
-  list->address = 0;
-  if (i && program->index)
-    {
-      d = ((tmp->address == - 1) ? app->programs->processes->pc : tmp->address)
-	- app->programs[program->index - 1].processes->pc;
-      list->address = app->programs[program->index - 1].processes->pc
-	+ ((d <= 0) * MEM_SIZE + d) / (i + 1);
-    }
-  return (address_check(app, program, list));
-}
-
 t_err		read_until(unsigned char *target, int fd,
 			   int size, char *file_name)
 {
